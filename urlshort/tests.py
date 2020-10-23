a2fc5a38209dbf063c6019c40e673ddb112e7d6b
alireza-mint-konk <alirezakhoramimn@gmail.com>
2020-10-23 13:52:51 +0330

    added the requirements

--- urlshort/tests.py
+++ urlshort/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
 # Create your tests here.
+ fixture from ``.
+import json
+import pytest
+from graphene_django.utils.testing import graphql_query
+
+@pytest.fixture
+def client_query(client):
+    def func(*args, **kwargs):
+        return graphql_query(*args, **kwargs, client=client)
+
+    return func
+
+# Test you query using the client_query fixture
+def test_some_query(client_query):
+    response = client_query(
+        '''
+        query {
+            urls {
+                id
+                full
+                hashedFull
+            }
+        }
+        ''',
+        op_name='urls'
+    )
+
+    content = json.loads(response.content)
+    assert 'یک جایی داری اشتباه میزنی داوش' not in content
