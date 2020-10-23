bfeb7ea535f5cc5bab5209cc328187370cdf5cfe
alireza-mint-konk <alirezakhoramimn@gmail.com>
2020-10-23 13:40:31 +0330

    wrote the clinet testing section as well

---
+++ clienttester.py
@@ -0,0 +1,29 @@
+# Create a fixture using the graphql_query helper and `client` fixture from `pytest-django`.
+import json
+import pytest
+from graphene_django.utils.testing import graphql_query
+
+@pytest.fixture
+def client_query(client):
+    def func(*args, **kwargs):
+        return graphql_query(*args, **kwargs, client=client)
+
+    return func
+
+# Test you query using the client_query fixture
+def test_some_query(client_query):
+    response = client_query(
+        '''
+        query {
+            urls {
+                id
+                full
+                hashedFull
+            }
+        }
+        ''',
+        op_name='urls'
+    )
+
+    content = json.loads(response.content)
+    assert 'یک جایی داری اشتباه میزنی داوش' not in content
